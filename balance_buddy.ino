e4f0c0dedf76b5222f7ec9df6d995790138cebae
Mitch Lustig <Mitchlol@users.noreply.github.com>
2021-01-09 04:01:18 -0800

    Rig up basic display

--- balance_buddy.ino
+++ balance_buddy.ino
@@ -3,7 +3,7 @@
 #include "balance_leds.cpp"
 
 ESC esc;
-BalanceDisplay balanceDisplay(esc);
+BalanceDisplay balanceDisplay;
 BalanceBeeper balanceBeeper;
 BalanceLEDs balanceLEDs;
 
@@ -20,8 +20,8 @@ void setup() {
 
 void loop() {
   esc.loop();
-  balanceDisplay.loop();
+  balanceDisplay.loop(esc.tempMosfet, esc.dutyCycle, esc.voltage, esc.balanceState, esc.switchState, esc.adc1, esc.adc2);
   balanceBeeper.loop(esc.dutyCycle, esc.erpm, esc.switchState, esc.voltage);
   balanceLEDs.loop(esc.erpm);
-
+  // No delay? #YOLO
 }
--- balance_display.cpp
+++ balance_display.cpp
@@ -6,33 +6,80 @@
 class BalanceDisplay {
   private:
     SSD1306AsciiWire oled;
-    ESC &esc;
   public:
-    BalanceDisplay(ESC &esc): esc(esc){};
     void setup(){
       Wire.begin();
       Wire.setClock(400000L);
       
       oled.begin(&Adafruit128x64, 0x3c);
-      oled.setFont(Adafruit5x7);
     }
 
-    void loop(){
+    void loop(double tempMosfet, double dutyCycle, double voltage, uint16_t balanceState, uint16_t switchState, double adc1, double adc2){
+     
       oled.home();
-      oled.set2X();
+
+      // Line 1: Voltage and Temp
+      oled.setFont(ZevvPeep8x16);
+      oled.set1X();
+      oled.print(voltage);
+      oled.print("V");
+      oled.print("  ");
+      oled.print(tempMosfet);
+      oled.println("c");
       
-      oled.print("P: ");
-      oled.print(esc.pitch);
-      oled.println("    ");
-    
-      oled.print("R: ");
-      oled.print(esc.roll);
-      oled.println("    ");
+      // Line 2: ADC Switches
+      oled.setFont(Adafruit5x7);
+      oled.set1X();
+      oled.print("ADC1: ");
+      oled.print(adc1);
+      oled.set2X();
+      if(switchState == 0){
+        oled.print(" OFF");
+      }else if(switchState == 1){
+        oled.print(" HALF");
+      }else{
+        oled.print(" ON");
+      }
+      oled.set1X();
+      oled.println();
+      oled.print("ADC2: ");
+      oled.println(adc2);
 
-      oled.print(esc.voltage);
-      oled.println("V    ");
+      // Line 3: Balance state
+      oled.setFont(TimesNewRoman13);
+      if(balanceState == 0){
+        oled.println("Calibrating             ");
+      }else if(balanceState == 1){
+        oled.println("Running                 ");
+      }else if(balanceState == 2){
+        oled.println("Run: Tiltback Duty      ");
+      }else if(balanceState == 3){
+        oled.println("Run: Tiltback HV        ");
+      }else if(balanceState == 4){
+        oled.println("Run: Tiltback LV        ");
+      }else if(balanceState == 5){
+        oled.println("Run: Tilt Constant      ");
+      }else if(balanceState == 6){
+        oled.println("Fault: Pitch Angle      ");
+      }else if(balanceState == 7){
+        oled.println("Fault: Roll Angle       ");
+      }else if(balanceState == 8){
+        oled.println("Fault: Switch Half      ");
+      }else if(balanceState == 9){
+        oled.println("Fault: Switch Full      ");
+      }else if(balanceState == 10){
+        oled.println("Fault: Duty             ");
+      }else if(balanceState == 11){
+        oled.println("Initial                 ");
+      }else{
+        oled.println("Unknown");
+      }
 
-      oled.print(esc.erpm);
-      oled.println(" <-ADC1    ");
+      // Line 4: Duty Cycle (up to 64 verical bars)
+      oled.setFont(TimesNewRoman16_bold);
+      for(float i = 0; i < fabsf(dutyCycle); i += 0.015){
+        oled.print("|");
+      }
+      oled.println();
     }
 };
