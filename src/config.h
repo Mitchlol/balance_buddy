f9a267acb9a130021cc2410ace005771298e605e
Carlin Hefner <carlinohefner@gmail.com>
2021-05-18 09:19:38 -0400

    deconflict pins in config and more formatting

--- src/config.h
+++ src/config.h
@@ -1,5 +1,7 @@
-// balance_beeper
-#define BEEPER_PIN 18
+/*----------------------------------------------------------------------------------------
+Settings for balance_beeper
+*/
+#define BEEPER_PIN 27
 
 #define PLAY_STARTUP true
 #define DUTY_CYCLE_ALERT 0.80          // 0 to disable
@@ -7,32 +9,41 @@
 #define LOW_VOLTAGE 40.0               // 0 to disable
 #define LOW_VOLTAGE_INTERVAL 30 * 1000 // every 30 seconds
 
-//----------------------------------------------------------------------------------------
-// balance_display
-#define USE_I2C // comment this out if using SPI
+/*----------------------------------------------------------------------------------------
+Settings for balance_display
+*/
+
+/* Uncomment below if using I2C, otherwise use SPI
+i2c is very slow, using it will cause inconsistent beeper timings
+long i2c wires are not recommended and may cause crashing */
+
+// #define USE_I2C
 
 #ifdef USE_I2C
 #include <Wire.h>
 #include "SSD1306AsciiWire.h"
+// other pins: SDA=21, SCL=22
 #else
 #include <SPI.h>
 #include "SSD1306AsciiSpi.h"
 #define CS_PIN 5
 #define DC_PIN 16
 #define RES_PIN 17
+// other pins: SCK=18, D1(MOSI)=23
 #endif
 
 #define REFRESH_INTERVAL 100 // 100ms = 0.1s = 10hz
 
-//----------------------------------------------------------------------------------------
-// balance_leds
+/*----------------------------------------------------------------------------------------
+Settings for balance_leds
+*/
 #define LED_TYPE WS2812
 #define COLOR_ORDER GRB
 #define BRIGHTNESS 64
 
 // Note: Forward in this instance means postive ERPM, which can actually be backwards if your motor direction is reversed.
-#define LED_PIN_FORWARD 19
-#define LED_PIN_BACKWARD 21
+#define LED_PIN_FORWARD 32
+#define LED_PIN_BACKWARD 33
 #define NUM_LEDS_FORWARD 1
 #define NUM_LEDS_BACKWARD 1
 #define STARTUP_FORWARD true
@@ -44,10 +55,11 @@
 #define COLOR_FORWARD CRGB::White
 #define COLOR_BACKWARD CRGB::Red
 
-//----------------------------------------------------------------------------------------
-// esc
+/*----------------------------------------------------------------------------------------
+Settings for esc
+*/
 #define ESC_CAN_ID 2
 #define BALANCE_BUDDY_CAN_ID 3
 
-#define CAN_RX_PIN 4
-#define CAN_TX_PIN 5
+#define CAN_RX_PIN 12
+#define CAN_TX_PIN 13
